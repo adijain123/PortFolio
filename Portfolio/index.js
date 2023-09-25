c9a3a47afe55becbe6e27319190fb257efd6d10a
Adi Jain <121253354+adijain123@users.noreply.github.com>
2023-09-26 01:05:28 +0530

    Add files via upload

---
+++ Portfolio/index.js
@@ -0,0 +1,11 @@
+function scrollValue() {
+    var navbar = document.getElementById('navbar');
+    var scroll = window.scrollY;
+    if (scroll < 200) {
+        navbar.classList.remove('BgColour');
+    } else {
+        navbar.classList.add('BgColour');
+    }
+}
+
+window.addEventListener('scroll', scrollValue);
